1b475a284fe0a6d2ee2195dc50218420b791640c
paiva-pedro <54715174+paiva-pedro@users.noreply.github.com>
2019-09-04 17:01:24 -0300

    Update IndicatorTable.js

--- IndicatorTable.js
+++ IndicatorTable.js
@@ -3,14 +3,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Paper, Grid } from '@material-ui/core';
 import Cart from '../images/cart.png';
+import ExpansionPanel from './ExpansionPanel';
+import Button from '@material-ui/core/Button';
+
 
 
 const useStyles = makeStyles({
     boxPrincipal: {
         width: '600px',
-        height: '600px',
+        height: '580px',
         background: '#fff',
         borderRadius: '0px',
+        overflow: 'hidden',
     },
     boxTitle: {
         display: 'flex',
@@ -33,6 +37,29 @@ const useStyles = makeStyles({
             marginTop: '5px',
         }    
     },
+    boxIndicator: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
+        margin: '20px auto',
+        '& p': {
+            fontSize: '30px',
+            fontWeight: '900',
+        }
+    },
+    button: {
+        backgroundColor: '#ffa100',
+        color: '#fff',
+        width: '180px',
+        height: '40px',
+        textTransform: 'uppercase',
+        fontSize: '18px',
+        fontWeight: '700',
+        '&:hover': {
+            backgroundColor: '#FF650D'
+        }
+    }
 });
 
 export default function MediaCard(props) {
@@ -40,11 +67,34 @@ export default function MediaCard(props) {
 
   return (
     <div>
-        <Paper className={classes.boxPrincipal}>
+        <Paper xs={12} className={classes.boxPrincipal}>
             <Grid className={classes.boxTitle} container>
                 <img src={props.icon? props.icon : Cart}></img>
                 <Typography>{props.title? props.title : "Titulo"}</Typography>
             </Grid>
+
+            <Grid className={classes.boxIndicator} container>
+                <Typography>1</Typography>
+                <ExpansionPanel/>
+            </Grid>
+            <Grid className={classes.boxIndicator} container>
+                <Typography>2</Typography>
+                <ExpansionPanel/>
+            </Grid>
+            <Grid className={classes.boxIndicator} container>
+                <Typography>3</Typography>
+                <ExpansionPanel/>
+            </Grid>
+            <Grid className={classes.boxIndicator} container>
+                <Typography>4</Typography>
+                <ExpansionPanel/>
+            </Grid>
+            <Grid className={classes.boxIndicator} container>
+                <Typography>5</Typography>
+                <ExpansionPanel/>
+            </Grid>
+
+            <Button variant="contained" className={classes.button}>Ver todos</Button>
         </Paper>
     </div> 
   );
